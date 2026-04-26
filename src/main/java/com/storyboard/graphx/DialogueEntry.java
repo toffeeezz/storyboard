32f7722bfb6abcaf6ba5a18066c8a5f7bb47c493
ttoaffers <animeone56@gmail.com>
2026-04-27 04:14:56 +0800

    added a delete button

--- src/main/java/com/storyboard/graphx/DialogueEntry.java
+++ src/main/java/com/storyboard/graphx/DialogueEntry.java
@@ -1,12 +1,41 @@
 package com.storyboard.graphx;
 
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import org.kordamp.ikonli.javafx.FontIcon;
 
-public class DialogueEntry extends StackPane {
+import java.io.IOException;
 
+public class DialogueEntry extends VBox {
+
+    public String character;
+    public String dialogue;
+
+    @FXML private FontIcon deleteButton;
 
     public DialogueEntry(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/DialogueEntry.fxml"));
 
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        deleteButton.setCursor(Cursor.HAND);
+
+        deleteButton.setOnMousePressed(e -> {
+            if(!(getParent() instanceof  VBox dialogueNode))
+                return;
+            dialogueNode.getChildren().remove(this);
+            System.out.println("remove");
+        });
     }
 }
