719fb44e25569b17f1dd483f67777c0da78c65a7
ttoaffers <animeone56@gmail.com>
2026-04-28 19:24:50 +0800

    added a getter for the character and dialogue fields

--- src/main/java/com/storyboard/graphx/DialogueEntry.java
+++ src/main/java/com/storyboard/graphx/DialogueEntry.java
@@ -4,7 +4,8 @@ package com.storyboard.graphx;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import org.kordamp.ikonli.javafx.FontIcon;
 
@@ -12,10 +13,24 @@ import java.io.IOException;
 
 public class DialogueEntry extends VBox {
 
-    public String character;
-    public String dialogue;
+    private String character;
+    private String dialogue;
+
+    public String getDialogue() {
+        dialogue = dialogueField.getText();
+        return dialogue;
+    }
+
+    public String getCharacter() {
+        character = characterField.getText();
+        return character;
+    }
+
+
 
     @FXML private FontIcon deleteButton;
+    @FXML private TextField characterField;
+    @FXML private TextArea dialogueField;
 
     public DialogueEntry(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/DialogueEntry.fxml"));
@@ -29,8 +44,10 @@ public class DialogueEntry extends VBox {
             throw new RuntimeException(e);
         }
 
-        deleteButton.setCursor(Cursor.HAND);
+        dialogue = dialogueField.getText();
+        character = characterField.getText();
 
+        deleteButton.setCursor(Cursor.HAND);
         deleteButton.setOnMousePressed(e -> {
             if(!(getParent() instanceof  VBox dialogueNode))
                 return;
