be6dff4f6a285debc28ad73843ce5a6151b5ba9c
ttoaffers <animeone56@gmail.com>
2026-04-30 04:21:02 +0800

    -fixed the bug of the arrow of children still attached to its parent when deleted -revised the retrieval of dialogues -names can now be set for each node

--- src/main/java/com/storyboard/graphx/DialogueNode.java
+++ src/main/java/com/storyboard/graphx/DialogueNode.java
@@ -1,13 +1,11 @@
 package com.storyboard.graphx;
 
+import com.storyboard.logic.Dialogue;
 import com.storyboard.utils.Vector2;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.VBox;
@@ -21,23 +19,20 @@ import java.util.List;
 
 public class DialogueNode extends StoryNode {
 
-    private final double defaultW = 300;
-    private final double defaultH = 200;
-
     private final List<DialogueEntry> entryList = new ArrayList<>();
 
-    public String getCompiledDialogue() {
-        compileDialogues();
-        return compiledDialogue;
-    }
-
-    protected String compiledDialogue;
-
+    @FXML private TextField nameField;
     @FXML private VBox entryPane;
     @FXML private FontIcon addButton;
 
-    @FXML private TextField textField;
-    @FXML private AnchorPane topBar;
+    public List<Dialogue> getDialogues(){
+        List<Dialogue> dialogues = new ArrayList<>();
+
+        for(DialogueEntry entry : entryList)
+            dialogues.add(new Dialogue(entry.getCharacter(), entry.getDialogue()));
+
+        return dialogues;
+    }
 
     public DialogueNode(Editor editor){
         super(editor);
@@ -50,16 +45,7 @@ public class DialogueNode extends StoryNode {
         setDefault();
     }
 
-
-
-
     private void setDefault(){
-        setPrefSize(defaultW, defaultH);
-        setBackground(Background.fill(Color.BLACK));
-        setBorder(Border.stroke(Color.WHITE));
-        getStyleClass().add("dialogue-card");
-
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/DialogueNode.fxml"));
         loader.setRoot(this);
         loader.setController(this);
@@ -70,39 +56,24 @@ public class DialogueNode extends StoryNode {
             throw new RuntimeException(e);
         }
 
-
+        setId(nameField.getText());
+        setBackground(Background.fill(Color.BLACK));
+        setBorder(Border.stroke(Color.WHITE));
+        getStyleClass().add("dialogue-card");
 
         origin = new Vector2(this.getPrefWidth() / 2, this.getPrefHeight() / 2);
         addButton.setCursor(Cursor.HAND);
-        addButton.setOnMousePressed(e -> {
-            addEntry(new DialogueEntry(this));
-        });
+        addButton.setOnMousePressed(_ -> addEntry(new DialogueEntry(this)));
         setOnKeyPressed(this::onKeyPressed);
     }
 
-    public void removeEntry(DialogueEntry entry){
+    protected void removeEntry(DialogueEntry entry){
         entryList.remove(entry);
         entryPane.getChildren().remove(entry);
     }
 
-    protected void addEntry(DialogueEntry entry){
+    private void addEntry(DialogueEntry entry){
         entryList.add(entry);
         entryPane.getChildren().add(entry);
-    }
-    @Override
-    protected void onKeyPressed(KeyEvent event){
-        super.onKeyPressed(event);
-        if(event.getCode() == KeyCode.ENTER && event.isShiftDown()){
-            compileDialogues();
-        }
-        event.consume();
-    }
-
-    private void compileDialogues(){
-        StringBuilder sb = new StringBuilder();
-        for(DialogueEntry entry : entryList)
-            sb.append("Character: ").append(entry.getCharacter()).append("\nDialogue: ").append(entry.getDialogue()).append("\n");
-        compiledDialogue = sb.toString().trim();
-        System.out.println(compiledDialogue);
     }
 }
