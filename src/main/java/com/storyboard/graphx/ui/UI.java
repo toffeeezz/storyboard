d12054ac7737829a821e39a87b93724dd3dd9ada
ttoaffers <animeone56@gmail.com>
2026-05-01 07:30:55 +0800

    init commit

---
+++ src/main/java/com/storyboard/graphx/ui/UI.java
@@ -0,0 +1,36 @@
+package com.storyboard.graphx.ui;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class UI extends StackPane {
+
+    public static UI instance;
+
+    private final EditorUI editorUI = new EditorUI();
+
+
+    public UI(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/UI.fxml"));
+
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        instance = this;
+        toggleEditor();
+    }
+
+
+    public void toggleEditor(){
+        if(getChildren().contains(editorUI))
+            getChildren().remove(editorUI);
+        else
+            getChildren().add(editorUI);
+    }
+}
