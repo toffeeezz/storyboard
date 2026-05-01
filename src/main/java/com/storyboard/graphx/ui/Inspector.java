a6434f3d5a58dc98764b6607adbd8c7fdb899cec
ttoaffers <animeone56@gmail.com>
2026-05-01 10:05:36 +0800

    name and parent labels now listen properly

--- src/main/java/com/storyboard/graphx/ui/Inspector.java
+++ src/main/java/com/storyboard/graphx/ui/Inspector.java
@@ -4,14 +4,18 @@ import com.storyboard.graphx.StoryNode;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class Inspector extends VBox {
 
     @FXML private Label parentLabel;
     @FXML private Label nameLabel;
+
+    private Consumer<StoryNode> onParentLabelPressed;
 
     protected Inspector(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/ui/Inspector.fxml"));
@@ -29,13 +33,28 @@ public class Inspector extends VBox {
     protected void showProperties(StoryNode node, boolean toggle){
         if(toggle){
             String name = node.getId();
-            String parentName = node.getParentNode() != null ? node.getParentNode().getId() : "";
+            StoryNode parent = node.getParentNode();
+            String parentName = parent != null ? node.getParentNode().getId() : "";
 
+            node.idProperty().addListener(_ -> {
+                String newName = node.getId();
+                nameLabel.setText(newName);
+            });
             nameLabel.setText(name);
             parentLabel.setText(parentName);
+            parentLabel.setOnMousePressed(_ -> {
+                if(onParentLabelPressed != null)
+                    onParentLabelPressed.accept(parent);
+            });
         }else{
             nameLabel.setText("");
             parentLabel.setText("");
         }
     }
+
+    protected void setOnParentLabelPressed(Consumer<StoryNode> callback){
+        this.onParentLabelPressed = callback;
+    }
+
+
 }
