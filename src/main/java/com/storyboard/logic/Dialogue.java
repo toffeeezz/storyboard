88808df2fe66b98f4e7ae7323125c9571fa61ace
ttoaffers <animeone56@gmail.com>
2026-05-02 01:57:36 +0800

    -converted the class to a record

--- src/main/java/com/storyboard/logic/Dialogue.java
+++ src/main/java/com/storyboard/logic/Dialogue.java
@@ -1,21 +1,5 @@
 package com.storyboard.logic;
 
-public final class Dialogue {
-    private String speaker;
-    private String text;
+-public record Dialogue(String speaker, String text) {
 
-
-    public String getSpeaker() {
-        return speaker;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-
-    public Dialogue(String speaker, String text){
-        this.speaker = speaker;
-        this.text = text;
-    }
 }
