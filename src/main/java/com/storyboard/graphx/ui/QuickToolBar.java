1bf9ed69017e10c79a5663fbb61a00860af62ebc
ttoaffers <animeone56@gmail.com>
2026-05-02 01:59:29 +0800

    init commit

---
+++ src/main/java/com/storyboard/graphx/ui/QuickToolBar.java
@@ -0,0 +1,34 @@
+package com.storyboard.graphx.ui;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class QuickToolBar extends VBox {
+
+    @FXML private Button addDialogueButton;
+
+    public QuickToolBar(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/ui/QuickToolBar.fxml"));
+
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    protected void onDialogueButtonPressed(EventHandler<MouseEvent> e){
+        addDialogueButton.setOnMousePressed(e);
+    }
+}
