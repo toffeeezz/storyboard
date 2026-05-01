29625b3657214ae68954991a22f65e21dc9d706d
ttoaffers <animeone56@gmail.com>
2026-05-02 01:59:22 +0800

    removed unused import

--- src/main/java/com/storyboard/graphx/ui/Inspector.java
+++ src/main/java/com/storyboard/graphx/ui/Inspector.java
@@ -4,7 +4,6 @@ import com.storyboard.graphx.StoryNode;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
