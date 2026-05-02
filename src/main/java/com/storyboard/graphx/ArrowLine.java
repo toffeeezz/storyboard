90359437b2cfbd64f476624f78f04cbf4e9b26a4
ttoaffers <animeone56@gmail.com>
2026-05-03 06:25:47 +0800

    revised the whole class to now accept vectors instead of nodes for broader utility

--- src/main/java/com/storyboard/graphx/ArrowLine.java
+++ src/main/java/com/storyboard/graphx/ArrowLine.java
@@ -1,15 +1,14 @@
 package com.storyboard.graphx;
 
 import com.storyboard.utils.Vector2;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.DoubleBinding;
-import javafx.event.Event;
-import javafx.event.EventHandler;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,111 +16,80 @@ import java.util.List;
 public class ArrowLine {
 
 
-    public final DoubleBinding angle;
-    public final DoubleBinding hypLen;
-    public final DoubleBinding endpointX;
-    public final DoubleBinding endpointY;
+    public final SimpleDoubleProperty angle;
+    public final Vector2 endPoint;
+    public final Vector2 startPoint;
+    public final Vector2 dist;
 
-    public final List<Node> shapes = new ArrayList<>();
-
-    protected StoryNode child;
-    protected StoryNode node;
+    public final List<Shape> shapes = new ArrayList<>();
 
 
-    public ArrowLine(double arrowSize, StoryNode child, StoryNode node){
-        this.child = child;
-        this.node = node;
+    private final Line line;
+    private final Polygon head;
 
-        DoubleBinding childX = child.layoutXProperty().add(child.widthProperty().divide(2));
-        DoubleBinding childY = child.layoutYProperty().add(child.heightProperty().divide(2));
-        DoubleBinding nodeX = node.layoutXProperty().add(node.widthProperty().divide(2));
-        DoubleBinding nodeY = node.layoutYProperty().add(node.heightProperty().divide(2));
 
-        //Create the line and bind each point from the child to the child
-        Line line = new Line();
-        line.startXProperty().bind(nodeX);
-        line.startYProperty().bind(nodeY);
-        line.endYProperty().bind(childY);
+
+    public ArrowLine(double arrowSize, Vector2 start, Vector2 end) {
+
+
+        angle = new SimpleDoubleProperty();
+        endPoint = new Vector2();
+        startPoint = new Vector2();
+        dist = new Vector2();
+
+        dist.x.addListener(_ -> updateAngle());
+        dist.y.addListener(_ -> updateAngle());
+
+        startPoint.x.bind(start.x);
+        startPoint.y.bind(start.y);
+
+        bindEndpoint(end);
+
+        line = new Line();
+        line.startXProperty().bind(startPoint.x);
+        line.startYProperty().bind(startPoint.y);
         line.setStrokeWidth(5);
         line.setStroke(Color.WHITE);
         line.setViewOrder(Editor.lineViewOrder);
+        line.endYProperty().bind(endPoint.y);
+        line.endXProperty().bind(endPoint.x);
 
-
-        Polygon head = new Polygon(
+        head = new Polygon(
                 0, 0,        //Tip
                 -arrowSize, -arrowSize / 2,    // Left
                 -arrowSize, arrowSize / 2      // Right
         );
-
-        head.layoutXProperty().bind(line.endXProperty().add(arrowSize / 2));
-        head.layoutYProperty().bind(line.endYProperty());
-        head.setFill(Color.WHITE);
         head.setViewOrder(Editor.arrowViewOrder);
-
-        //Calculate the distance between the two points of the line
-        Vector2 dist = new Vector2();
-        dist.x.bind(childX.subtract(nodeX));
-        dist.y.bind(childY.subtract(nodeY));
-        hypLen = Bindings.createDoubleBinding(() -> {
-            double dx = dist.getX();
-            double dy = dist.getY();
-            return Math.sqrt(dx * dx + dy * dy);
-        }, dist.x, dist.y);
-
-        //Calculate the angle of the line
-        angle = Bindings.createDoubleBinding(() -> {
-            double dx = dist.getX();
-            double dy = dist.getY();
-
-            double radians = Math.atan2(dy, dx);
-            return Math.toDegrees(radians);
-        }, dist.x, dist.y);
-
-        //Calculate the X endpoint based on the angle of the arrow
-        endpointX = Bindings.createDoubleBinding(() -> {
-            double w;
-            double degrees = Math.abs(angle.get());
-            double totalW = child.getWidth();
-
-            w = degrees * (totalW / 180);
-
-            return child.getLayoutX() + w;
-
-        }, child.widthProperty(), child.layoutXProperty(), angle);
-
-        endpointY = Bindings.createDoubleBinding(() -> {
-            double radians = Math.toRadians(angle.get());
-            double totalH = child.getHeight();
-
-            double h = (totalH / 2) - (Math.sin(radians) * (totalH / 2));
-
-            return child.getLayoutY() + h;
-
-        }, child.heightProperty(), child.layoutYProperty(), angle);
-
-        line.endXProperty().bind(endpointX);
-        line.endYProperty().bind(endpointY);
-
-
-        angle.addListener(_ -> System.out.println("Angle: " + angle.get()));
-
+        head.setFill(Color.WHITE);
+        head.layoutXProperty().bind(line.endXProperty());
+        head.layoutYProperty().bind(line.endYProperty());
         head.rotateProperty().bind(angle);
 
         shapes.add(line);
         shapes.add(head);
-
-        addOnMousePressed(_ -> System.out.println("Clicked!!"));
     }
 
-    private void addOnMousePressed(EventHandler<MouseEvent> event){
-        for(Node node : shapes){
-            node.setOnMousePressed(e -> {
-                event.handle(e);
-                e.consume();
-            });
+    private void updateAngle(){
+        double dx = dist.getX();
+        double dy = dist.getY();
+        double radians = Math.atan2(dy, dx);
+        angle.set(Math.toDegrees(radians));
+        System.out.println(Math.toDegrees(radians));
+    }
 
-            node.setOnMouseDragged(Event::consume);
+    public void bindEndpoint(Vector2 point){
+        if(endPoint.x.isBound() && endPoint.y.isBound()){
+            endPoint.x.unbind();
+            endPoint.y.unbind();
         }
-    }
+        endPoint.x.bind(point.x);
+        endPoint.y.bind(point.y);
 
+        if(dist.x.isBound() && dist.y.isBound()){
+            dist.x.unbind();
+            dist.y.unbind();
+        }
+        dist.x.bind(endPoint.x.subtract(startPoint.x));
+        dist.y.bind(endPoint.y.subtract(startPoint.y));
+    }
 }
