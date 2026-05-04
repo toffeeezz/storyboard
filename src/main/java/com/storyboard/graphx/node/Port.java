f1a2b83187314d9ef8f42e16b3e53bed410673b7
ttoaffers <animeone56@gmail.com>
2026-05-05 05:52:23 +0800

    added a boolean property to detect links

--- src/main/java/com/storyboard/graphx/node/Port.java
+++ src/main/java/com/storyboard/graphx/node/Port.java
@@ -2,10 +2,21 @@ package com.storyboard.graphx.node;
 
 import com.storyboard.utils.Vector2;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Circle;
 
 public class Port extends Circle {
+
+    private final SimpleBooleanProperty linkedProperty = new SimpleBooleanProperty();
+
+    public boolean isLinkedProperty() {
+        return linkedProperty.get();
+    }
+
+    public SimpleBooleanProperty linkedPropertyProperty() {
+        return linkedProperty;
+    }
 
     public StoryNode getStoryNode() {
         return storyNode;
@@ -35,6 +46,13 @@ public class Port extends Circle {
             Point2D worldPos = storyNode.getEditor().getWorldPane().sceneToLocal(scenePos);
             return worldPos.getY();
         }, layoutYProperty(), storyNode.layoutYProperty()));
+
+        linkedProperty.addListener((_, _, isLinked) -> {
+            if(isLinked)
+                getStyleClass().add("active");
+            else
+                getStyleClass().remove("active");
+        });
 
         return endCenter;
     }
